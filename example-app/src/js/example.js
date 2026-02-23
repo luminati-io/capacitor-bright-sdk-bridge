ba558e59586d4ba46ceab5dd12c708310b415acb
Dmitrii Shelonin <dmitriis@brightdata.com>
2026-02-23 15:46:53 +0200

    - actualized example app

--- example-app/src/js/example.js
+++ example-app/src/js/example.js
@@ -1,6 +1,40 @@
+import '../style.css'
 import { BrightSdkBridge } from 'capacitor-bright-sdk-bridge';
 
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    BrightSdkBridge.echo({ value: inputValue })
+const versionEl = document.querySelector('#sdk-version');
+const uuidEl = document.querySelector('#sdk-uuid');
+const choiceEl = document.querySelector('#sdk-current-choice');
+const showConsentBtn = document.querySelector('#show-consent-btn');
+const optOutBtn = document.querySelector('#optout-btn');
+
+function handleChoice(choice) {
+  choiceEl.textContent = choice;
+  if (choice == 1) {
+    showConsentBtn.style.display = 'none';
+    optOutBtn.style.display = '';
+  } else {
+    showConsentBtn.style.display = '';
+    optOutBtn.style.display = 'none';
+  }
 }
+
+BrightSdkBridge.addListener('onChoiceChange', (event) => {
+  handleChoice(event.value);
+});
+
+showConsentBtn.addEventListener('click', async () => {
+  await BrightSdkBridge.showConsent({
+    benefit: 'Demo benefit',
+  });
+});
+
+optOutBtn.addEventListener('click', async () => {
+  BrightSdkBridge.optOut();
+});
+
+const {value: choice} = await BrightSdkBridge.currentChoice();
+const {value: version} = await BrightSdkBridge.version();
+const {value: uuid} = await BrightSdkBridge.uuid();
+versionEl.textContent = version;
+uuidEl.textContent = uuid;
+handleChoice(choice);
